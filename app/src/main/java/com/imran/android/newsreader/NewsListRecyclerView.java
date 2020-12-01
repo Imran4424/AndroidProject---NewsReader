8d33e4e3100f259f9da6bf2f74107f87ddaa50d3
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-01 23:59:42 +0600

    Recycler view

--- app/src/main/java/com/imran/android/newsreader/NewsListRecyclerView.java
+++ app/src/main/java/com/imran/android/newsreader/NewsListRecyclerView.java
@@ -1,7 +1,35 @@
 package com.imran.android.newsreader;
 
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 /**
  * Created by Shah Md Imran Hossain on 01, December, 2020
  */
-public class NewsListRecyclerView {
+public class NewsListRecyclerView extends RecyclerView.Adapter<NewsListRecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public NewsListRecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsListRecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
