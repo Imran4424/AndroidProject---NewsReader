b7ea7376a924530649596f40337342f67b32f4cc
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-04 03:20:10 +0600

    model

--- app/src/main/java/com/imran/android/newsreader/model/ArticleDao.java
+++ app/src/main/java/com/imran/android/newsreader/model/ArticleDao.java
@@ -1,7 +1,34 @@
 package com.imran.android.newsreader.model;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.imran.android.newsreader.Constants;
+
+import java.util.List;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
 /**
  * Created by Shah Md Imran Hossain on 04, December, 2020
  */
+
+@Dao
 public interface ArticleDao {
+    // insert query
+    @Insert(onConflict = REPLACE)
+    void insert(ArticleData articleData);
+
+    @Delete
+    void delete(ArticleData articleData);
+
+    // delete all query
+    @Delete
+    void reset(List<ArticleData> articleDataList);
+
+    // get all data query
+    @Query("SELECT * FROM" + Constants.TABLE_NAME_ARTICLE)
+    List<ArticleData> getAll();
 }
