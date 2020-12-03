d4fe80b50638803320f2499d1ac002fbd8afa9c2
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-04 02:49:32 +0600

    model

--- app/src/main/java/com/imran/android/newsreader/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.imran.android.newsreader;
-
-/**
- * Created by Shah Md Imran Hossain on 04, December, 2020
- */
-public class Constant {
-}
---
+++ app/src/main/java/com/imran/android/newsreader/Constants.java
@@ -0,0 +1,9 @@
+package com.imran.android.newsreader;
+
+/**
+ * Created by Shah Md Imran Hossain on 04, December, 2020
+ */
+public class Constants {
+    // model constant
+    public static final String TABLE_NAME_ARTICLE = "ArticleTable";
+}
