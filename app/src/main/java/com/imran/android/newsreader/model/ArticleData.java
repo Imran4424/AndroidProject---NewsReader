0a3ed012c610bdb03b36d2f0f94f0080911003fd
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-04 02:44:41 +0600

    model

--- app/src/main/java/com/imran/android/newsreader/model/ArticleData.java
+++ app/src/main/java/com/imran/android/newsreader/model/ArticleData.java
@@ -1,10 +1,14 @@
 package com.imran.android.newsreader.model;
 
+import androidx.room.Entity;
+
 import java.io.Serializable;
 
 /**
  * Created by Shah Md Imran Hossain on 04, December, 2020
  */
+
+@Entity(tableName = "")
 public class ArticleData implements Serializable {
 
 }
