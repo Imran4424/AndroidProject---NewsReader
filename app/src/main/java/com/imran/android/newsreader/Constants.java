07966d50561bfa4806dc67c83d5c8a9db7ccf3ed
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-04 02:53:11 +0600

    model

--- app/src/main/java/com/imran/android/newsreader/Constants.java
+++ app/src/main/java/com/imran/android/newsreader/Constants.java
@@ -4,6 +4,7 @@ package com.imran.android.newsreader;
  * Created by Shah Md Imran Hossain on 04, December, 2020
  */
 public class Constants {
-    // model constant
+    // model constants
     public static final String TABLE_NAME_ARTICLE = "ArticleTable";
+    public static final String DB_NAME = "articleDB";
 }
--- app/src/main/java/com/imran/android/newsreader/model/ArticleData.java
+++ app/src/main/java/com/imran/android/newsreader/model/ArticleData.java
@@ -2,13 +2,15 @@ package com.imran.android.newsreader.model;
 
 import androidx.room.Entity;
 
+import com.imran.android.newsreader.Constants;
+
 import java.io.Serializable;
 
 /**
  * Created by Shah Md Imran Hossain on 04, December, 2020
  */
 
-@Entity(tableName = "")
+@Entity(tableName = Constants.TABLE_NAME_ARTICLE)
 public class ArticleData implements Serializable {
 
 }
