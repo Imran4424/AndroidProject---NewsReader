ad723661392146a4690249bbdc48646471b79c52
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-06 13:09:59 +0600

    article activity

--- app/src/main/java/com/imran/android/newsreader/ArticleActivity.java
+++ app/src/main/java/com/imran/android/newsreader/ArticleActivity.java
@@ -3,6 +3,7 @@ package com.imran.android.newsreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class ArticleActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class ArticleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
+
+        WebView articleWebView = findViewById(R.id.articleWebView);
     }
 }
